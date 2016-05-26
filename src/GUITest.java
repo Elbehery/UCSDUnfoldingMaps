d9561b1baf8fe2a6e2cf9a4d7267ba6926b2762e
elbehery <eng.m.behery@live.com>
2016-05-26 15:13:53 +0200

    first

---
+++ src/GUITest.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class GUITest extends PApplet{
+	
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		super.draw();
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		GUITest guiTest = new GUITest();
+		
+	}
+
+}
